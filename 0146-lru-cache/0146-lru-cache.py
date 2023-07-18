a2fc618e7a5b7f500e1e6fdfcba21f5426713b40
atul-chaturvedi <44370008+atul-chaturvedi@users.noreply.github.com>
2023-07-18 23:24:29 +0530

    Time: 693 ms (95.09%), Space: 78 MB (67.38%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,86 +1,32 @@
-class LRUCache {
-public:
-    class Node{
-        public:
-        int key;
-        int val;
-        Node* next;
-        Node* prev;
+class LRUCache:
+    def __init__(self, capacity):
+        self.dict = {}
+        self.freeSpace = capacity
 
-        Node(int k, int v){
-            this->key = k;
-            this->val = v;
-        }
-    };
 
-    Node* head = new Node(-1, -1);
-    Node* tail = new Node(-1, -1);
-    int cap;
-    unordered_map<int, Node*> m;
+    def get(self, key):
+        if key not in self.dict:
+            return -1
 
-    LRUCache(int capacity) {
-        cap = capacity;
-        head->next = tail;
-        tail->prev = head;
-    }
+        self.dict[key] = self.dict.pop(key)
 
-    // add at the last
-    void addNode(Node* newNode){
-        Node* temp = head->next;
-        newNode->next = temp;
-        newNode->prev = head;
+        return self.dict[key]
 
-        head->next = newNode;
-        temp->prev = newNode;
-    }
 
-    // removing last used
-    void deleteNode(Node * delNode){
-        Node* prevv = delNode->prev;
-        Node* nextt = delNode->next;
+    def put(self, key, value):
+        if key in self.dict:
+            self.dict.pop(key)
+        else:
+            if self.freeSpace:
+                self.freeSpace -= 1
+            else:
+                self.dict.pop(next(iter(self.dict)))
 
-        prevv ->next = nextt;
-        nextt->prev = prevv;
+        self.dict[key] = value
+        
 
-    }
 
-    
-    int get(int key) {
-        if(m.find(key) != m.end()){
-            Node* resNode = m[key];
-            int ans = resNode->val;
-
-            m.erase(key);
-            deleteNode(resNode);
-            addNode(resNode);
-
-            m[key] = head->next;
-            return ans;
-        }
-        return -1;
-    }
-    
-    void put(int key, int value) {
-       if(m.find(key) != m.end()){
-           Node* curr = m[key];
-           m.erase(key);
-           deleteNode(curr);
-       }
-
-       if(m.size() == cap){
-           m.erase(tail->prev ->key);
-           deleteNode(tail->prev);
-       }
-
-       addNode(new Node(key, value));
-       m[key] = head->next;
-
-    }
-};
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache* obj = new LRUCache(capacity);
- * int param_1 = obj->get(key);
- * obj->put(key,value);
- */
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
