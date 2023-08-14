fc5032ff6fe460726f258384f2bf93c7d78ba1fc
atul-chaturvedi <44370008+atul-chaturvedi@users.noreply.github.com>
2023-08-14 23:27:53 +0530

    Time: 37 ms (20.15%), Space: 18 MB (24.35%) - LeetHub

--- 2811-check-if-it-is-possible-to-split-array/2811-check-if-it-is-possible-to-split-array.cpp
+++ 2811-check-if-it-is-possible-to-split-array/2811-check-if-it-is-possible-to-split-array.cpp
@@ -107,12 +107,61 @@ public:
 // This case is only possible if there are two consecutive number whose sum is greater than or equal to m. Because if we have this case then we can simply remove all previous and post numbers one by one. But if this is not present then there is no way we can devide all the elements as at some point we arrive at point where we have three numbers left and non of the two consicutive sum greater than or equal to m.
 
 // Note : corner case - if we have less and 2 elements then it is always possible to divide it into two as each have a length of 1.
-bool canSplitArray(vector<int>& nums, int m) {
-    if(nums.size() <= 2) return true;
-    for(int i = 1; i < nums.size(); i++){
-        if(nums[i] + nums[i-1] >= m) return true;
-    }
-    return false;
-}
+// bool canSplitArray(vector<int>& nums, int m) {
+//     if(nums.size() <= 2) return true;
+//     for(int i = 1; i < nums.size(); i++){
+//         if(nums[i] + nums[i-1] >= m) return true;
+//     }
+//     return false;
+// }
     
+    
+// ---------------------------------------------------Approach 2---------------------------------------------------
+// Intuition
+//  -->We try to make partition at every index, if we can partition following the two conditions, rest recursion will handle.
+
+// Approach
+//  ->Partiton after each index, check if partition is correct (fulfills both the conditions)
+// -->Check recursively on the two partitions made for the same conditions
+// -->Apply memoization
+
+    int dp[101][101];
+    vector<int> prefixSum;
+    int solve(int i, int j, vector<int>&nums, int m) {
+        if (i == j) return 1;
+        if (dp[i][j] != -1) return dp[i][j];
+
+        int sum = 0;
+        if (i == 0) sum = prefixSum[j];
+        else sum = prefixSum[j] - prefixSum[i - 1];
+
+        if (j - i == 1 && sum >= m) return dp[i][j] = 1;
+        if (sum < m) return dp[i][j] = false;
+
+        dp[i][j] = false;
+        for (int k = i; k < j; k++) {
+            if (solve(i, k, nums, m) == 1 && solve(k + 1, j, nums, m) == 1) {
+                return dp[i][j] = (dp[i][j] || true);
+            }
+        }
+        return dp[i][j];
+    }
+
+    bool canSplitArray(vector<int>& nums, int m) {
+        int n = nums.size();
+        memset(dp, -1, sizeof(dp));
+        prefixSum.assign(n, 0);
+        prefixSum[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + nums[i];
+        }
+        if (n == 2) return true;
+        return solve(0, n - 1, nums, m);
+    }
 };
+    
+
+
+
+
+
