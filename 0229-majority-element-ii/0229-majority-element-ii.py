4c37c35b98197db1449e49b54c73fea79a1e632f
atul-chaturvedi <44370008+atul-chaturvedi@users.noreply.github.com>
2024-03-08 22:41:32 +0530

    Time: 98 ms (62.55%), Space: 17.9 MB (76.40%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,17 +1,25 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        count = dict()
-        for num in nums:
-            if num not in count:
-                count[num] =1
-            else:
-                count[num] +=1
-        ans = []
-        for c in count.keys():
-            if count[c] > n//3:
-                ans.append(c)
+#         n = len(nums)
+#         count = dict()
+#         for num in nums:
+#             if num not in count:
+#                 count[num] =1
+#             else:
+#                 count[num] +=1
+#         ans = []
+#         for c in count.keys():
+#             if count[c] > n//3:
+#                 ans.append(c)
                 
-        return ans
+#         return ans
+        
+        # This is how we have to aim for in python
+        # list to map with frequency using Counter function
+        # Counter(nums).items() to iterate keys and values
+        # condtion v i,e value if greatet than len(nums)// 3
+        # taking k i,e key when condition is true
+        
+        return [ k for k, v in Counter(nums).items() if v > len(nums) // 3 ]
             
         
