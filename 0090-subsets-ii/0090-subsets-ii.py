c2770f36644ee9a85464c6e266f7d73cc5dfaecc
atul-chaturvedi <44370008+atul-chaturvedi@users.noreply.github.com>
2024-03-10 22:37:14 +0530

    Time: 39 ms (59.76%), Space: 16.9 MB (31.09%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,23 +1,28 @@
-class Solution {
-public:
-    set<vector<int>> ans;
-    void help(vector<int>& nums, int i, int n, vector<int> curr_subs){
-        if(i>= n){
-            ans.insert(curr_subs);
-            return;
-        }
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        ans = []
+        curr_subset = []
+
+        def findSubsets(i):
+            if i>= len(nums):
+                ans.append(curr_subset[:])
+                return;
+            
+            # print(curr_subset)
+            curr_subset.append(nums[i]);
+            findSubsets(i+1)
+            curr_subset.pop(-1)
+            while(i+1  < len(nums) and nums[i] == nums[i+1]):
+                i+=1
+            
+            findSubsets(i+1)
+            
+    
+
+        findSubsets(0)
+
+        return ans
         
-        help(nums, i+1, n, curr_subs);
-        curr_subs.push_back(nums[i]);
-        help(nums, i+1, n, curr_subs);
-        
-        // 0 {}
-        
-    }
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        help(nums, 0, nums.size(), {});
-        vector<vector<int>> fans(ans.begin(), ans.end());
-        return fans;
-    }
-};
+    
+    
