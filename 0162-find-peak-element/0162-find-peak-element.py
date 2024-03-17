c11f1d758f45763618aa415036d3a4e338d06557
atul-chaturvedi <44370008+atul-chaturvedi@users.noreply.github.com>
2024-03-17 21:39:59 +0530

    Time: 48 ms (48.20%), Space: 17 MB (8.61%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -1,14 +1,32 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         # brute force approach
-        index = -1
-        max_element = -2147483649
+#         index = -1
+#         max_element = -2147483649
         
-        for i in range(len(nums)):
-            if nums[i] > max_element:
-                max_element = nums[i]
-                index = i
+#         for i in range(len(nums)):
+#             if nums[i] > max_element:
+#                 max_element = nums[i]
+#                 index = i
                 
         
-        return index
+#         return index
+
+        #optimal approch using binary search
+        low = 0
+        high = len(nums) -1
+        
+        while(low <= high):
+            mid = (low+high)//2
+            
+            if mid - 1 >= low and  nums[mid] < nums[mid -1]:
+                high = mid -1
+            elif mid +1 <= high and nums[mid] < nums[mid +1]:
+                low = mid + 1
+                
+            else:
+                return mid
+        
+        return -1
+            
         
