a132d6c457669f97c6af67cb379c9059ca0155f3
atul-chaturvedi <44370008+atul-chaturvedi@users.noreply.github.com>
2024-03-22 00:34:01 +0530

    Time: 32 ms (95.63%), Space: 16.8 MB (64.87%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.py
@@ -0,0 +1,8 @@
+import sys 
+sys.set_int_max_str_digits(0)
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        return str(int(num1) + int(num2))
+            
+            
+        
